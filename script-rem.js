3f434f5895f9c8522536a600c0c75d5a96b1a7a6
TDNM88 <167914317+TDNM88@users.noreply.github.com>
2024-06-12 09:06:48 +0700

    Update script-rem.js

--- script-rem.js
+++ script-rem.js
@@ -7,13 +7,14 @@ MIIEpAIBAAKCAQEAufVBBTEHKYB4wHB/XqxTFZBJ+Qv2C1TYkaYR73nr37Zj5COW3M9ZzT8nKanx+cgQ
 3bw5cGAILrZitaFvNYINQhdKd/Cfpyvavd33wDa1q01Q7v3SLkzkKH3Jszqc7YAUF7HETunuVljtRrfPjQjVuKgGzEK0iAJh2k8FqQIDAQABAoIBAGhusJ4+w3vuN2Izi4RamKpihviDXCFO+/UH48iumuXfI0grKH1HQXJ/xfmjBE63qbDKXNU7Ew6FplhPf6Ihs9DB4m1BOO1eY/QwNAT9sT7AG68NMBAmASobW4tFD6hP6T79K7KRQC+x41Sl4R5/ditgp0lUWuBY3aFrY/hsJjiSkyWACum/8DuyHWJYd1rtrYTHXk+fdMZGDk4/+G4xFkbBcq7/VCe4+HsxOLV7gEQ9WmkpzVtEluT75LRbX5bwFpW74AUj10GkKNXqKBn6RaeyRL4DQLAYVM7rsK+AHF+VOF+tJ1LbRHmoJV09/ok9J5IttdVfs4TLW6zy+PhTr6UCgYEA4iRe23rO9PuOGkrwPLkBylJcezYLqIMb8EuJPbsEkPtJgxkOZz2SB8oN6u/PiZIjlfKrYb433D13YDjbdesZXxO+bt0ph2Yg9gxViZ5v4KW8xj+q2Ez+REBKu+cJoQoLlhkzFtJNGXQjFx1Z4LLPdiHBDhuhjAsuhGMmbhRRzX8CgYEA0oKoYFldtYcgIndmIqWloTCR0EofZuw0GNTV5Qj3zT6YWPg9rXX/K7rZXm/HS5FlbxioWvJQKUygA6Pcv2kpoWZ9LmqT4RfsF/YkoS43Chb6amShY2NXoGiv+tdlguJABPrWgg9WW4NKx+kkzjgqa/B/o8/8L53bTx1ybgP8kNcCgYEAsy0KE3BMhgLCZsa1bgdQCdQT/nOl7RBklJHk3YpOZekUJ9E0fkxkZdOMD3eOwnuNwoMRk1I5xEDXUt/50oZCBfkIy6JkAynCN/Ex3OYx0pa+6X0MinEGjWE7VYILORNSdszkk9Jw0BgarXfN/KDwi7LW2ay+5Mz/QFmm21UyM1UCgYEAuhUdWe4lU+3ajh0Vf9MFknMosgKTt5tcggbB /O/sfNW0PhFSFB+WLMXCmCQe+8HO9GHLkHlaOuetGwqie+EuF6pNRdaYWgWXgabnwkLkDFs7iQjjUMPkFbv0s7vr08LD7KL/M4DzSXnJhOWmz1P+y+OQMPINbtoPnuPc3ztO64kCgYBJktd41VVvCj+zLnfSi/pru3dfcpqjKEBricWcHpqsqjyOFlqmdZx/mt5g9EZ0gEdpG5vZrPVLN8d9mNL8dJKbOhN4sMYcXAsNLTUqst9+TPnpNtGizU6PXi7C3KLS7Q7hDe2uiwRu7lIuPffeHm0ZkgNGzFRsL/Vrm/6eWCuFdA==
 -----END PRIVATE KEY-----`;
 
+
 async function generateSignature(method, url, appId, body) {
     const timestamp = Math.floor(Date.now() / 1000);
     const message = `${method}${url}${timestamp}${appId}${body ? JSON.stringify(body) : ''}`;
-    const sign = crypto.createSign('RSA-SHA256');
-    sign.update(message);
-    sign.end();
-    const signature = sign.sign(PRIVATE_KEY, 'base64');
+    const rsa = new KJUR.crypto.Signature({ "alg": "SHA256withRSA" });
+    rsa.init(PRIVATE_KEY);
+    rsa.updateString(message);
+    const signature = rsa.sign();
     return `TAMS-HMAC-SHA256 ${timestamp}:${signature}`;
 }
 
